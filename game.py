f77637fc8d088382eac8fa8ffe89554a352ec007
Alexandru Jurju <alexandru.jurju@ulbisbiu.ro>
2023-02-13 13:08:09 +0200

    centered neurons

--- game.py
+++ game.py
@@ -469,29 +469,54 @@ class Game:
         neuron_offset_y = ViewConsts.NN_DISPLAY_OFFSET_Y
         neuron_positions: List[Tuple[int, int]] = []
 
+        max_y = -1
+        for layer in dense_layers:
+            max_y_input = layer.input_size * (ViewConsts.NN_DISPLAY_NEURON_HEIGHT_BETWEEN + ViewConsts.NN_DISPLAY_NEURON_RADIUS * 2)
+            max_y_output = layer.output_size * (ViewConsts.NN_DISPLAY_NEURON_HEIGHT_BETWEEN + ViewConsts.NN_DISPLAY_NEURON_RADIUS * 2)
+
+            max_layer = max_y_input if max_y_input > max_y_output else max_y_output
+
+            if max_layer > max_y:
+                max_y = max_layer
+
+        print(max_y)
+
         for layer_count, layer in enumerate(dense_layers):
             input_neuron_count = layer.input_size
             output_neuron_count = layer.output_size
 
             if layer_count == 0:
-                # draw input and output neurons
+                current_max_y = input_neuron_count * (ViewConsts.NN_DISPLAY_NEURON_HEIGHT_BETWEEN + ViewConsts.NN_DISPLAY_NEURON_RADIUS * 2)
+                offset = (max_y - current_max_y) // 2
+                neuron_offset_y += offset
+
                 for i in range(input_neuron_count):
                     neuron_x = neuron_offset_x
                     neuron_y = neuron_offset_y
                     neuron_offset_y += ViewConsts.NN_DISPLAY_NEURON_HEIGHT_BETWEEN + ViewConsts.NN_DISPLAY_NEURON_RADIUS * 2
                     neuron_positions.append((neuron_x, neuron_y))
+
                 neuron_offset_x += ViewConsts.NN_DISPLAY_NEURON_WIDTH_BETWEEN
                 neuron_offset_y = ViewConsts.NN_DISPLAY_OFFSET_Y
 
+                current_max_y = output_neuron_count * (ViewConsts.NN_DISPLAY_NEURON_HEIGHT_BETWEEN + ViewConsts.NN_DISPLAY_NEURON_RADIUS * 2)
+                offset = (max_y - current_max_y) // 2
+                neuron_offset_y += offset
+                
                 for j in range(output_neuron_count):
                     neuron_x = neuron_offset_x
                     neuron_y = neuron_offset_y
                     neuron_offset_y += ViewConsts.NN_DISPLAY_NEURON_HEIGHT_BETWEEN + ViewConsts.NN_DISPLAY_NEURON_RADIUS * 2
                     neuron_positions.append((neuron_x, neuron_y))
+
                 neuron_offset_x += ViewConsts.NN_DISPLAY_NEURON_WIDTH_BETWEEN
                 neuron_offset_y = ViewConsts.NN_DISPLAY_OFFSET_Y
 
             else:
+                current_max_y = output_neuron_count * (ViewConsts.NN_DISPLAY_NEURON_HEIGHT_BETWEEN + ViewConsts.NN_DISPLAY_NEURON_RADIUS * 2)
+                offset = (max_y - current_max_y) // 2
+                neuron_offset_y += offset
+
                 for j in range(output_neuron_count):
                     neuron_x = neuron_offset_x
                     neuron_y = neuron_offset_y
