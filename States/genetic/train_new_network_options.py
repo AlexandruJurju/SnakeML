f6aa4cb643ea97780475e605f80e6e275998337f
Alexandru Jurju <alexandru.jurju@ulbisbiu.ro>
2023-02-17 08:41:52 +0200

    genetic options file name

--- States/genetic/train_new_network_options.py
+++ States/genetic/train_new_network_options.py
@@ -39,6 +39,9 @@ class GeneticTrainNetworkOptions(BaseState):
         self.starting_snake_size_entry = None
         self.starting_snake_size_entry_label = None
 
+        self.file_name_entry = None
+        self.file_name_label = None
+
         self.button_run = None
 
     def start(self):
@@ -47,6 +50,10 @@ class GeneticTrainNetworkOptions(BaseState):
 
         self.dropdown_input_direction_count = UIDropDownMenu(NNSettings.AVAILABLE_INPUT_DIRECTIONS, NNSettings.AVAILABLE_INPUT_DIRECTIONS[0], pygame.Rect((200, 350), (75, 30)), self.ui_manager)
         self.dropdown_input_direction_count_label = UILabel(pygame.Rect((115, 300), (250, 35)), "Input Direction Count", self.ui_manager)
+
+        self.file_name_entry = UITextEntryLine(pygame.Rect((410, 350), (125, 30)), self.ui_manager)
+        self.file_name_label = UILabel(pygame.Rect((405, 300), (125, 35)), "Network name", self.ui_manager)
+        self.file_name_entry.set_text("Default")
 
         self.dropdown_vision_line_return_type = UIDropDownMenu(NNSettings.AVAILABLE_VISION_LINES_RETURN_TYPE, NNSettings.AVAILABLE_VISION_LINES_RETURN_TYPE[0], pygame.Rect((650, 350), (100, 30)), self.ui_manager)
         self.dropdown_vision_line_return_type_label = UILabel(pygame.Rect((565, 300), (250, 35)), "Vision Line Return Type", self.ui_manager)
